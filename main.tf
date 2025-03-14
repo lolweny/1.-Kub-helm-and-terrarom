f2dd316224725c3d820c3d9a27396329fa79aab1
lanceolweny <lanceolweny@gmail.com>
2025-03-13 21:35:45 -0500

    Adding project files after cleanup

---
+++ main.tf
@@ -0,0 +1,33 @@
+provider "azurerm" {
+  features {}
+
+  subscription_id = "ca6f5a82-c6a1-4540-8f4f-b33db838cf9c"
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "k8s-resource-group"
+  location = "East US"
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "aks-cluster"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "myakscluster"
+
+  default_node_pool {
+    name       = "agentpool"
+    node_count = 2
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+output "kube_config" {
+  value     = azurerm_kubernetes_cluster.aks.kube_config_raw
+  sensitive = true
+}
+
