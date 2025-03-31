ca65cb4b6ebf912a689ff3fc769b156f2c9adba4
lanceolweny <lanceolweny@gmail.com>
2025-03-30 23:39:16 -0500

    chore: update app version to v8

--- my-app/app.py
+++ my-app/app.py
@@ -1,10 +1,14 @@
 from flask import Flask
+from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
 
+# ✅ Explicitly bind metrics to /metrics
+metrics = PrometheusMetrics(app, path="/metrics")
+
 @app.route("/")
 def hello():
-    return "👋 Hello from Lance AKS!"
+    return "👋 Hello from Lance AKS! - v8"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
